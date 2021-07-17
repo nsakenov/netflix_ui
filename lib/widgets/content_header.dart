2fa35fef0dd476185eb4d34e9badd108ac4d25a7
nurcity <nurbol.sakenov@outlook.com>
2021-07-17 20:21:20 +0600

    web support wip

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_ui/models/models.dart';
 import 'package:netflix_ui/widgets/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content featuredContent;
   ContentHeader({required this.featuredContent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _ContentHeaderMobile(featuredContent: featuredContent),
+      desktop: _ContentHeaderDesktop(featuredContent: featuredContent),
+    );
+  }
+}
+
+class _ContentHeaderMobile extends StatelessWidget {
+  final Content featuredContent;
+  _ContentHeaderMobile({required this.featuredContent});
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +72,148 @@ class ContentHeader extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class _ContentHeaderDesktop extends StatefulWidget {
+  final Content featuredContent;
+  _ContentHeaderDesktop({required this.featuredContent});
+
+  @override
+  __ContentHeaderDesktopState createState() => __ContentHeaderDesktopState();
+}
+
+class __ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
+  late VideoPlayerController _videoPlayerController;
+  bool _isMuted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.network(
+        widget.featuredContent.videoUrl ?? 'url_placeholder')
+      ..initialize().then(
+        (_) {
+          setState(() {});
+        },
+      )
+      ..setVolume(0);
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _videoPlayerController.value.isPlaying
+            ? _videoPlayerController.pause()
+            : _videoPlayerController.play();
+      },
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          AspectRatio(
+            aspectRatio: _videoPlayerController.value.isInitialized
+                ? _videoPlayerController.value.aspectRatio
+                : 2.344,
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Image.asset(
+                    widget.featuredContent.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          Container(
+            height: 500.0,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.black, Colors.transparent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter),
+            ),
+          ),
+          Positioned(
+            left: 60.0,
+            right: 60.0,
+            bottom: 150.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250.0,
+                  child: Image.asset(widget.featuredContent.titleImageUrl ??
+                      'placeholder_url'),
+                ),
+                const SizedBox(height: 15.0),
+                Text(
+                  widget.featuredContent.description ?? 'placeholder_title',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          offset: Offset(2.0, 4.0),
+                          blurRadius: 6.0),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    _PlayButton(),
+                    const SizedBox(width: 16.0),
+                    TextButton(
+                      onPressed: () => print('more info'),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.info_outline,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 5.0),
+                          Text(
+                            'More Info',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 20.0),
+                    if (_videoPlayerController.value.isInitialized)
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isMuted
+                                ? _videoPlayerController.setVolume(100)
+                                : _videoPlayerController.setVolume(0);
+                            _isMuted = _videoPlayerController.value.volume == 0;
+                          });
+                        },
+                        icon: Icon(
+                          _isMuted ? Icons.volume_off : Icons.volume_up,
+                          color: Colors.white,
+                          size: 30.0,
+                        ),
+                      ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
